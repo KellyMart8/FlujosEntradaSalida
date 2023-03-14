a2d272b9bd5a126973cb9dc29c124f523e4317bf
JADPA-29 <JADPA-29@DESKTOP-3A50JMP>
2023-03-14 08:28:19 -0600

    Agregar archivos de proyecto.

---
+++ ConsultaCredito/frmconsultaCredito.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ConsultaCredito
+{
+    partial class frmconsultaCredito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMostrar = new System.Windows.Forms.RichTextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnCredito = new System.Windows.Forms.Button();
+            this.btnDebito = new System.Windows.Forms.Button();
+            this.btnCero = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMostrar
+            // 
+            this.txtMostrar.Location = new System.Drawing.Point(25, 27);
+            this.txtMostrar.Name = "txtMostrar";
+            this.txtMostrar.Size = new System.Drawing.Size(746, 251);
+            this.txtMostrar.TabIndex = 0;
+            this.txtMostrar.Text = "";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(33, 308);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(120, 23);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir archivo";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnCredito
+            // 
+            this.btnCredito.Location = new System.Drawing.Point(182, 308);
+            this.btnCredito.Name = "btnCredito";
+            this.btnCredito.Size = new System.Drawing.Size(125, 23);
+            this.btnCredito.TabIndex = 2;
+            this.btnCredito.Text = "Saldos con credito";
+            this.btnCredito.UseVisualStyleBackColor = true;
+            this.btnCredito.Click += new System.EventHandler(this.ObtenerSaldo_Click);
+            // 
+            // btnDebito
+            // 
+            this.btnDebito.Location = new System.Drawing.Point(345, 308);
+            this.btnDebito.Name = "btnDebito";
+            this.btnDebito.Size = new System.Drawing.Size(114, 23);
+            this.btnDebito.TabIndex = 3;
+            this.btnDebito.Text = "Saldos con debito";
+            this.btnDebito.UseVisualStyleBackColor = true;
+            this.btnDebito.Click += new System.EventHandler(this.ObtenerSaldo_Click);
+            // 
+            // btnCero
+            // 
+            this.btnCero.Location = new System.Drawing.Point(501, 308);
+            this.btnCero.Name = "btnCero";
+            this.btnCero.Size = new System.Drawing.Size(102, 23);
+            this.btnCero.TabIndex = 4;
+            this.btnCero.Text = "Saldos en cero";
+            this.btnCero.UseVisualStyleBackColor = true;
+            this.btnCero.Click += new System.EventHandler(this.ObtenerSaldo_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(684, 308);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmconsultaCredito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 358);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCero);
+            this.Controls.Add(this.btnDebito);
+            this.Controls.Add(this.btnCredito);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.txtMostrar);
+            this.Name = "frmconsultaCredito";
+            this.Text = "Consulta de Credito";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox txtMostrar;
+        private Button btnAbrir;
+        private Button btnCredito;
+        private Button btnDebito;
+        private Button btnCero;
+        private Button btnSalir;
+    }
+}
---
+++ ConsultaCredito/frmconsultaCredito.cs
@@ -0,0 +1,158 @@
+﻿using BibliotecaBanco;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultaCredito
+{
+    public partial class frmconsultaCredito : Form
+    {
+        private FileStream entrada; //MAntiene la conexion con el archivo
+        private StreamReader archivoReader; //Leer los datos de texto
+
+        private string nombreArchivo;
+        public frmconsultaCredito()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectorArchivo = new OpenFileDialog();
+            DialogResult resultado = selectorArchivo.ShowDialog();
+
+            if (resultado == DialogResult.Cancel)
+                return;
+
+            nombreArchivo = selectorArchivo.FileName;
+
+            if (nombreArchivo == "" || nombreArchivo == null)
+            {
+                MessageBox.Show("Nombre del archivo invalido", "Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                entrada = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read);
+                archivoReader = new StreamReader(entrada);
+
+                btnAbrir.Enabled = false;
+                btnCredito.Enabled = true;
+                btnDebito.Enabled = true;
+                btnCero.Enabled = true;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (entrada != null)
+            {
+                try
+                {
+                    entrada.Close();
+                    archivoReader.Close();
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("No se pudo crear el archivo", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }Application.Exit();
+        }
+
+        //Se invoca cuando el usuario hace clic en el boton de saldo con credito,
+        //Saldo con debito o saldos en cero
+        private void ObtenerSaldo_Click(object sender, EventArgs e) 
+        {
+            //conviertee el emisor (sender) explicitamente a un objeto de tipo Button
+            Button emisorButton = (Button)sender;
+
+            //obtiene el texto del boton en el que se hizo clic, y que almacena el tipo 
+            //de la cuenta
+            string tipoCuenta = emisorButton.Text;
+
+            //leer y muestra la informacion del archivo
+
+
+            try
+            {
+                //regresa al principio
+                entrada.Seek(0, SeekOrigin.Begin);
+
+                txtMostrar.Text = "Las cuentasson:\r\n";
+
+                //Recorre el archivo hasta llegassr a su fin
+                while (true)
+                {
+                    string[] camposEntrada; //almacena piezas de datos indicviduales
+                    Registro registro;  //Almacena cada regiastro a medida que se lee el archivo
+                    decimal saldo;  //Almacena el saldo a cada Registro
+
+                    //obtiene el siguiente Registro disponible en el archivo
+                    string registroEntrada = archivoReader.ReadLine();
+
+                    //cuando esta al final del archivo, sale del metodo
+
+                    if (registroEntrada == null)
+                        return;
+
+                    camposEntrada = registroEntrada.Split(','); //Analiza la entrada
+
+                    //Crea el registro a partir de entrada
+                    registro = new Registro(Convert.ToInt16(camposEntrada[0]), camposEntrada[1], camposEntrada[2], Convert.ToDecimal(camposEntrada[3]));
+
+                    //Almacenar el ultimo campo del regisdtro en saldo
+                    saldo = registro.Saldo;
+
+                    //Metodo parta determinar si mueesra saldo o no
+                    if (DebeMostrar(saldo, tipoCuenta))
+                    {
+
+                        //muestra el refistro
+                        string salida = registro.Cuenta + "\t" + registro.PrimerNombre + "\t" +
+                            registro.ApellidoPaterno + "\t";
+
+                        //muiestra el saldo con el formato monetario correccto
+                        salida += String.Format("{0:F}", saldo) + "\r\n";
+
+                        txtMostrar.Text += salida;
+                    }
+                }
+            }
+            //maneja la excepcion cuando no puede leeerse el archivo
+            catch (IOException)
+            {
+                MessageBox.Show("No se pude leer el archivo", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool DebeMostrar(decimal saldo, string tipoCuenta)
+        {
+            if (saldo > 0)
+            {
+                //Muestra los saldos con credito
+                if (tipoCuenta == "Saldos con creditos")
+                    return true;
+
+            }else if (saldo < 0)
+            {
+                //muestra los valores con debitos
+                if (tipoCuenta == "Saldos con debito")
+                    return true;
+            }
+            else //saldo == 0
+            {
+                if (tipoCuenta == "Saldos en cero")
+                    return true;
+            }
+            return false;
+        }
+    }
+}
